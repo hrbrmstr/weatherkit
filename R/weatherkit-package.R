76954f7815a9623c55c34603d3e15de31fc3b28f
hrbrmstr <bob@rud.is>
2022-07-03 06:02:09 -0400

    R package repo initialization complete

---
+++ R/weatherkit-package.R
@@ -0,0 +1,10 @@
+#' ...
+#' 
+#' @md
+#' @name weatherkit
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+# @ useDynLib weatherkit, .registration = TRUE
+"_PACKAGE"
