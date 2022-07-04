064b55ff58b9aa935523165ef5772a0c0a21618b
hrbrmstr <bob@rud.is>
2022-07-04 08:24:33 -0400

    tidy the wx object

---
+++ R/tidy.R
@@ -0,0 +1,109 @@
+#' Tidy values in weather data
+#'
+#' @param wx object returned by [wxkit_weather()]
+#' @export
+#' @examples
+#' wx_tidy(wxkit_weather(43.2683199, -70.8635506))
+wx_tidy <- function(wx) {
+
+  if (hasName(wx, "foreCastDaily") &&
+      hasName(wx$forecastDaily, "days") &&
+      inherits(wx$forecastDaily$days, "data.frame")) {
+
+    c(
+      "forecastStart", "forecastEnd", "moonrise", "moonset",
+      "solarMidnight", "solarNoon", "sunrise", "sunriseCivil",
+      "sunriseNautical", "sunriseAstronomical", "sunset",
+      "sunsetCivil", "sunsetNautical", "sunsetAstronomical"
+    ) -> posixct_fields
+
+    for (field in posixct_fields) {
+      wx$forecastDaily$days[[field]] <- anytime::anytime(wx$forecastDaily$days[[field]])
+    }
+
+    if (hasName(wx$forecastDaily$days, "daytimeForecast") &&
+        inherits(wx$forecastDaily$days$daytimeForecast, "data.frame")) {
+
+      c(
+        "forecastStart", "forecastEnd"
+      ) -> posixct_fields
+
+      for (field in posixct_fields) {
+        wx$forecastDaily$days$daytimeForecast[[field]] <-
+          anytime::anytime(wx$forecastDaily$days$daytimeForecast[[field]])
+      }
+
+    }
+
+    if (hasName(wx$forecastDaily$days, "overnightForecast") &&
+        inherits(wx$forecastDaily$days$overnightForecast, "data.frame")) {
+
+      c(
+        "forecastStart", "forecastEnd"
+      ) -> posixct_fields
+
+      for (field in posixct_fields) {
+        wx$forecastDaily$days$overnightForecast[[field]] <-
+          anytime::anytime(wx$forecastDaily$days$overnightForecast[[field]])
+      }
+
+    }
+
+    if (hasName(wx$forecastDaily$days, "restOfDayForecast") &&
+        inherits(wx$forecastDaily$days$restOfDayForecast, "data.frame")) {
+
+      c(
+        "forecastStart", "forecastEnd"
+      ) -> posixct_fields
+
+      for (field in posixct_fields) {
+        wx$forecastDaily$days$restOfDayForecast[[field]] <-
+          anytime::anytime(wx$forecastDaily$days$restOfDayForecast[[field]])
+      }
+
+    }
+
+  }
+
+  if (hasName(wx, "forecastHourly") &&
+      hasName(wx$forecastHourly, "hours") &&
+      inherits(wx$forecastHourly$hours, "data.frame")) {
+
+    c(
+      "forecastStart"
+    ) -> posixct_fields
+
+    for (field in posixct_fields) {
+      wx$forecastHourly$hours[[field]] <-
+        anytime::anytime(wx$forecastHourly$hours[[field]])
+    }
+
+  }
+
+  if (hasName(wx, "forecastNextHour")) {
+
+    if (hasName(wx$forecastNextHour, "summary")) {
+      wx$forecastNextHour$summary$startTime <-
+        anytime::anytime(wx$forecastNextHour$summary$startTime)
+    }
+
+    if (hasName(wx$forecastNextHour, "forecastStart")) {
+      wx$forecastNextHour$forecastStart <-
+        anytime::anytime(wx$forecastNextHour$forecastStart)
+    }
+
+    if (hasName(wx$forecastNextHour, "forecastEnd")) {
+      wx$forecastNextHour$forecastEnd <-
+        anytime::anytime(wx$forecastNextHour$forecastEnd)
+    }
+
+    if (hasName(wx$forecastNextHour, "minutes")) {
+      wx$forecastNextHour$minutes$startTime <-
+        anytime::anytime(wx$forecastNextHour$minutes$startTime)
+    }
+
+  }
+
+  wx
+
+}
